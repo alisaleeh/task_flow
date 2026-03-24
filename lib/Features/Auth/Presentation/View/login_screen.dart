ff6b94d85cf6da8a55797a9ac3de8a3b2cb4a846
Ali Saleh <alisaleeh14@gmail.com>
2026-03-24 03:01:13 +0300

    refactor login screen to use state management and integrate snack bar notifications

--- lib/Features/Auth/Presentation/View/login_screen.dart
+++ lib/Features/Auth/Presentation/View/login_screen.dart
@@ -1,16 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:taskflow/Core/Constants/app_colors.dart';
 import 'package:taskflow/Core/Constants/app_routes.dart';
 import 'package:taskflow/Core/Constants/app_spacing.dart';
 import 'package:taskflow/Core/Constants/app_text_styles.dart';
+import 'package:taskflow/Core/Widgets/custom_snack_bar.dart';
+import 'package:taskflow/Features/Auth/Presentation/Manager/login_cubit/login_cubit.dart';
 import 'package:taskflow/Features/Auth/Presentation/View/Widgets/app_logo.dart';
 import 'package:taskflow/Features/Auth/Presentation/View/Widgets/bottom_action_text.dart';
 import 'package:taskflow/Features/Auth/Presentation/View/Widgets/custom_text_field.dart';
 import 'package:taskflow/Features/Auth/Presentation/View/Widgets/header_texts.dart';
 import 'package:taskflow/Features/Auth/Presentation/View/Widgets/primary_button.dart';
 
-class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+class LoginScreen extends StatefulWidget {
+  LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController emailcontroller = TextEditingController();
+
+  final TextEditingController passwordcontroller = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  @override
+  void dispose() {
+    emailcontroller.dispose();
+    passwordcontroller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,69 +38,107 @@ class LoginScreen extends StatelessWidget {
       body: SafeArea(
         top: false,
         bottom: true,
-        child: CustomScrollView(
-          physics: const BouncingScrollPhysics(),
-          slivers: [
-            SliverPadding(
-              padding: AppSpacing.screenPadding,
-              sliver: SliverFillRemaining(
-                hasScrollBody: false,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    AppSpacing.gapV20,
-                    const AppLogo(),
-                    AppSpacing.gapV24,
-                    const HeaderTexts(),
-                    AppSpacing.gapV48,
+        child: BlocConsumer<LoginCubit, LoginState>(
+          listener: (context, state) {
+            switch (state) {
+              case LoginInitial():
+              case LoginLoading():
+                break;
+              case LoginFailure():
+                CustomSnackBar.showError(context, state.errorMessage);
+                break;
+              case LoginSuccess():
+                CustomSnackBar.showSuccess(context, 'Login successful!');
+                Navigator.pushNamed(context, AppRoutes.home);
+                break;
+            }
+          },
+          builder: (context, state) {
+            return Form(
+              key: _formKey,
+              child: CustomScrollView(
+                physics: const BouncingScrollPhysics(),
+                slivers: [
+                  SliverPadding(
+                    padding: AppSpacing.screenPadding,
+                    sliver: SliverFillRemaining(
+                      hasScrollBody: false,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          AppSpacing.gapV20,
+                          const AppLogo(),
+                          AppSpacing.gapV24,
+                          const HeaderTexts(),
+                          AppSpacing.gapV48,
 
-                    const CustomTextField(
-                      label: 'Email',
-                      hintText: 'your@email.com',
-                      keyboardType: TextInputType.emailAddress,
-                      textinputaction: TextInputAction.next,
-                    ),
-                    AppSpacing.gapV20,
+                          CustomTextField(
+                            label: 'Email',
+                            hintText: 'your@email.com',
+                            keyboardType: TextInputType.emailAddress,
+                            textinputaction: TextInputAction.next,
+                            controller: emailcontroller,
+                          ),
+                          AppSpacing.gapV20,
 
-                    const CustomTextField(
-                      label: 'Password',
-                      hintText: '........',
-                      isPassword: true,
-                      textinputaction: TextInputAction.done,
-                    ),
-                    AppSpacing.gapV12,
+                          CustomTextField(
+                            label: 'Password',
+                            hintText: '........',
+                            isPassword: true,
+                            textinputaction: TextInputAction.done,
+                            controller: passwordcontroller,
+                          ),
+                          AppSpacing.gapV12,
 
-                    // زر نسيت كلمة المرور
-                    Align(
-                      alignment: Alignment.centerRight,
-                      child: GestureDetector(
-                        onTap: () {
-                          Navigator.pushNamed(
-                            context,
-                            AppRoutes.forgotPassword,
-                          );
-                        },
-                        child: Text(
-                          'Forgot Password?',
-                          style: AppTextStyles.font14SemiBoldOrange,
-                        ),
+                          // زر نسيت كلمة المرور
+                          Align(
+                            alignment: Alignment.centerRight,
+                            child: GestureDetector(
+                              onTap: () {
+                                Navigator.pushNamed(
+                                  context,
+                                  AppRoutes.forgotPassword,
+                                );
+                              },
+                              child: Text(
+                                'Forgot Password?',
+                                style: AppTextStyles.font14SemiBoldOrange,
+                              ),
+                            ),
+                          ),
+                          AppSpacing.gapV32,
+                          if (state is LoginLoading)
+                            const Center(
+                              child: CircularProgressIndicator(
+                                color: AppColors.primaryOrange,
+                              ),
+                            )
+                          else
+                            PrimaryButton(
+                              text: 'Sign In',
+                              onPressed: () {
+                                if (_formKey.currentState!.validate()) {
+                                  FocusScope.of(context).unfocus();
+                                  context.read<LoginCubit>().login(
+                                    emailcontroller.text.trim(),
+                                    passwordcontroller.text.trim(),
+                                  );
+                                }
+                              },
+                            ),
+
+                          AppSpacing.gapV24,
+
+                          const BottomActionText(),
+                          const Spacer(),
+                        ],
                       ),
                     ),
-                    AppSpacing.gapV32,
-
-                    PrimaryButton(text: 'Sign In', onPressed: () {
-                        Navigator.pushNamed(context, AppRoutes.mainLayout);
-                    }),
-
-                    AppSpacing.gapV24,
-
-                    const BottomActionText(),
-                    const Spacer(),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            ),
-          ],
+            );
+          },
         ),
       ),
     );
